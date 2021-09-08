a4b4f7e1f373d508400323fdab81343c2f023ece
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-09-08 16:14:35 -0300

    Req 9: Completo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -15,9 +15,9 @@ export default class AddMovie extends Component {
   }
 
   changeInput({ target }) {
-    const { value } = target;
+    const { value, name } = target;
     this.setState({
-      title: value,
+      [name]: value,
     });
   }
 
@@ -26,13 +26,25 @@ export default class AddMovie extends Component {
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="input-text" data-testid="title-input-label">
+        <label htmlFor="input-title" data-testid="title-input-label">
           <p>Título</p>
           <input
             type="text"
             data-testid="title-input"
             id="input-text"
+            name="title"
             value={ title }
+            onChange={ this.changeInput }
+          />
+        </label>
+        <label htmlFor="input-subtitle" data-testid="subtitle-input-label">
+          <p>Subtítulo</p>
+          <input
+            type="text"
+            data-testid="subtitle-input"
+            id="input-subtitle"
+            name="subtitle"
+            value={ subtitle }
             onChange={ this.changeInput }
           />
         </label>
