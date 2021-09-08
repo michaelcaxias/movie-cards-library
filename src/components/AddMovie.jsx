92a7ffda927bc4b576fd6e49ed06aea35b53d166
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-09-08 15:50:38 -0300

    Req 6: Completo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+export default class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form>
+        
+      </form>
+    );
+  }
+}
