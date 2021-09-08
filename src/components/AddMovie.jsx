903def9b941766df32ca2d62e8badc8a1de275c4
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-09-08 16:06:07 -0300

    Req 8: Completo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,13 +11,31 @@ export default class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+    this.changeInput = this.changeInput.bind(this);
+  }
+
+  changeInput({ target }) {
+    const { value } = target;
+    this.setState({
+      title: value,
+    });
   }
 
   render() {
     const { onClick } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-
+        <label htmlFor="input-text" data-testid="title-input-label">
+          <p>Título</p>
+          <input
+            type="text"
+            data-testid="title-input"
+            id="input-text"
+            value={ title }
+            onChange={ this.changeInput }
+          />
+        </label>
       </form>
     );
   }
